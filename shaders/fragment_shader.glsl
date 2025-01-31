e48d90fb457a95f8afe65849b25728dafdc14638
GH <garrett.hannah@ontariotechu.net>
2025-01-31 16:38:05 -0500

    Updated fragment shader (its a bit easier on the eyes...)

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -16,12 +16,22 @@ void main() {
     
     
     FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    float shade = FragNormal.y * 0.7;
-    shade = step(0.25, shade) * roundToTenth(shade);
-    FragColor -= 0.75 * vec4(shade, shade, shade, 0.0);
+    float shade = abs(FragNormal.y);
+        //(0.5 * sin(tcolor) + 0.5);
+    float bounceLight = -FragNormal.y * 0.15;
+
+    FragColor = vec4(bounceLight, bounceLight, bounceLight, 1.0);
+    
+    FragColor += vec4(shade, shade, shade, 0.0);
+
+    FragColor *= 0.50;
+    FragColor += 0.5;
+
+    FragColor *= vec4(0.89, 0.7, 0.7, 1);
+
+
+    //FragColor = 0.75 * vec4(shade, shade, shade, 0.0);
     
     float normVal = FragNormal.y;
 
-    
-    FragColor += vec4(0.25 * sin(tcolor * 0.2), 0.25 * sin(tcolor * 0.4), 0.25 * sin(tcolor), 0);
 }
